24e699feec061efce2b4f305d676588f9b8e77e4
satej <satejtupare@gmail.com>
2023-08-02 13:34:12 +0530

    2 Aug commit no.2

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 app=Flask(__name__)
 
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
 @app.route("/")
 def welcome():
     return "heyyyyyyyyyyyyyy"
--- model/user_model.py
+++ model/user_model.py
@@ -5,6 +5,7 @@ class user_model():
         try:
             self.con=mysql.connector.connect(host="localhost",user="root",password="Satej@892002",database="flask_project4")
             self.cur = self.con.cursor(dictionary=True)
+            self.con.autocommit=True
             print("connection is successful.")
         except:
             print("some error in sql connection.......")
@@ -17,6 +18,20 @@ class user_model():
             return "There is no data"
         
     def user_add_model(self,data):
-        print(data['email'])
-        return "this is user add new modedxlllllll"
+        self.cur.execute(f"INSERT INTO users(name,email,phone,role ,password) VALUES ('{data['name']}','{data['email']}','{data['phone']}','{data['role']}','{data['password']}')")
+        return "user created successfully in the database...."
     
+
+    def user_update_model(self,data):
+        self.cur.execute(f"UPDATE users SET name='{data['name']}', email='{data['email']}', role='{data['role']}',phone='{data['phone']}',password='{data['password']}' WHERE id={data['id']}")
+        if self.cur.rowcount>0:
+            return "The data is updated successfully........"
+        else:
+            return "The id is not there so the data is not updated......."
+
+    def user_delete_model(self,id):
+        self.cur.execute(f"DELETE FROM users WHERE id ={id}")
+        if self.cur.rowcount>0:
+            return "The data is deleted successfully........"
+        else:
+            return "The id is not there so the data is not delete......."  
